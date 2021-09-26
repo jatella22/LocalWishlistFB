7f85ae74484a2f7ebb31f7029f803fd59f6022cc
TELLA22 <tellajoseantonioxd@gmail.com>
2021-09-26 02:17:36 +0200

    JAT - 20210926 - First commit Front.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemComponent,
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent },
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
+      { path: 'item', component: ItemComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/item/model/item.ts
@@ -0,0 +1,6 @@
+export class Item {
+    public id: number = 0;
+    public name: string = "";
+    public link: string = "";
+    public price: number = 0.0;
+}
